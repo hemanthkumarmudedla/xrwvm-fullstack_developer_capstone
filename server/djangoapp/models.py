fa02186d57b758d8e0d19fc3b39e165698e54a00
hemanthkumarmudedla <hemanthkumarmudedla@gmail.com>
2024-07-24 19:49:57 +0530

    check-in for lints models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,14 +1,11 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    
     def __str__(self):
         return self.name
+
 
 class CarModel(models.Model):
     CAR_TYPE_CHOICES = [
@@ -16,14 +13,11 @@ class CarModel(models.Model):
         ('SUV', 'SUV'),
         ('WAG', 'Wagon'),
     ]
-
-
-    make = models.ForeignKey(CarMake, on_delete=models.CASCADE, 
-        related_name='car_models')
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE,
+    related_name='car_models')
     dealer_id = models.IntegerField()
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=3, choices=CAR_TYPE_CHOICES)
     year = models.IntegerField()
-    
     def __str__(self):
         return f"{self.make.name} {self.name}"
